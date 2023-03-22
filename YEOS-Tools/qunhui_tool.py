d2938e3fda484f2b86160ef081c76d7db72d1523
Scepter <scepter@1949hacker.cn>
2023-03-22 19:26:57 +0800

    backup

--- YEOS-Tools/qunhui_tool.py
+++ YEOS-Tools/qunhui_tool.py
@@ -76,7 +76,7 @@ def randwrite():
 
 # 创建读文件
 def create_readFile():
-    print("初始化读测试环境,至少需要,等待...")
+    print("初始化读测试环境,至少需要十几分钟甚至几十分钟,等着...")
     cmd = [
         "fio",
         "-name=create_read",
