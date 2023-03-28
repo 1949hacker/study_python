7e92cdc021f6f4005230c1be750f580fd87ef30d
Scepter <scepter@1949hacker.cn>
2023-03-28 18:08:59 +0800

    backup

--- YEOS-Tools/stressNAS.py
+++ YEOS-Tools/stressNAS.py
@@ -1,305 +1,74 @@
 #!/usr/bin/python3
 
-"""
-TODO:
-    自动进行压0.
-    测，路径为/mnt/test/stress
-    时.
-    间和盘位由用户决定
-"""
-
 import subprocess, re
 
 
-# 随机写
-def randwrite():
-    # 初始化用于存储运行结果的列表
-    bw = [0, 0, 0]
-    iops = [0, 0, 0]
-
-    # fio重复运行4次
-    print("随机写进行中...")
-    for i in range(4):
-        cmd = [
-            "fio",
-            "-name=YEOS",
-            "-size=32G",
-            "-runtime=120s",
-            "-bs=4k",
-            "-direct=1",
-            "-rw=randwrite",
-            "-ioengine=libaio",
-            "-numjobs=12",
-            "-group_reporting",
-            "-iodepth=64",
-            f"-filename=/dev/{disk}",
-        ]
-
-        # 将fio运行结果标准输出到管道
-        fio1 = subprocess.Popen(cmd, stdout=subprocess.PIPE, shell=False)
-        fio2 = subprocess.Popen(
-            ["grep", "samples"], stdin=fio1.stdout, stdout=subprocess.PIPE, shell=False
-        )
-
-        # 使用communicate获取子进程的标准输出并格式化为utf-8编码
-        fio = fio2.communicate()[0].decode("utf-8")
-
-        # 初始化列表
-        bw_num = []
-        iops_num = []
-
-        # 匹配数字和小数点，并将其元素更新
-        for line in fio.split("\n"):
-            if "bw" in line:
-                bw_num.extend(re.findall(r"\d+\.\d+|\d+", line))
-            elif "iops" in line:
-                iops_num.extend(re.findall(r"\d+\.\d+|\d+", line))
-
-        # 将str类型转换为float后再转换为int
-        bw_num = [int(float(e)) for e in bw_num]
-        iops_num = [int(float(e)) for e in iops_num]
-        # 跳过第一次运行结果
-        if i == 0:
-            continue
-        else:
-            # 格式化fio结果
-            for KorM in fio.split("\n"):
-                # 判断格式化的结果中是否存在MiB单位的值
-                if "MiB" in KorM:
-                    # 若有则转换为KiB
-                    # 读带宽
-                    bw[0] += bw_num[0] * 1024
-                    bw[1] += bw_num[1] * 1024
-                    bw[2] += bw_num[3] * 1024
-                    # 写带宽
-                    bw[3] += bw_num[6] * 1024
-                    bw[4] += bw_num[7] * 1024
-                    bw[5] += bw_num[9] * 1024
-                    # 读IOPS
-                    iops[0] += iops_num[0] * 1024
-                    iops[1] += iops_num[1] * 1024
-                    iops[2] += iops_num[2] * 1024
-                    # 写IOPS
-                    iops[3] += iops_num[5] * 1024
-                    iops[4] += iops_num[6] * 1024
-                    iops[5] += iops_num[7] * 1024
-                else:
-                    # 读带宽
-                    bw[0] += bw_num[0]
-                    bw[1] += bw_num[1]
-                    bw[2] += bw_num[3]
-                    # 写带宽
-                    bw[3] += bw_num[6]
-                    bw[4] += bw_num[7]
-                    bw[5] += bw_num[9]
-                    # 读IOPS
-                    iops[0] += iops_num[0]
-                    iops[1] += iops_num[1]
-                    iops[2] += iops_num[2]
-                    # 写IOPS
-                    iops[3] += iops_num[5]
-                    iops[4] += iops_num[6]
-                    iops[5] += iops_num[7]
-
-    bwMin = int(bw[0] / 3)
-    bwMax = int(bw[1] / 3)
-    bwAvg = int(bw[2] / 3)
-    iopsMin = int(iops[0] / 3)
-    iopsMax = int(iops[1] / 3)
-    iopsAvg = int(iops[2] / 3)
-
-    print(
-        f"随机写均值如下:\n带宽最小值:{bwMin},最大值{bwMax},均值{bwAvg}\nIOPS最小值:{iopsMin},"
-        f"最大值{iopsMax},均值{iopsAvg}"
-    )
-
-
-# 随机读
-def randread():
-    # 初始化用于存储运行结果的列表
-    bw = [0, 0, 0]
-    iops = [0, 0, 0]
-    # fio重复运行4次
-    print("随机读进行中...")
-
-    for i in range(4):
-        cmd = [
-            "fio",
-            "-name=YEOS",
-            "-size=32G",
-            "-runtime=120s",
-            "-bs=4k",
-            "-direct=1",
-            "-rw=randwrite",
-            "-ioengine=libaio",
-            "-numjobs=12",
-            "-group_reporting",
-            "-iodepth=64",
-            f"-filename=/dev/{disk}",
-        ]
-
-        # 将fio运行结果标准输出到管道
-        fio1 = subprocess.Popen(cmd, stdout=subprocess.PIPE, shell=False)
-        fio2 = subprocess.Popen(
-            ["grep", "samples"], stdin=fio1.stdout, stdout=subprocess.PIPE, shell=False
-        )
-
-        # 使用communicate获取子进程的标准输出并格式化为utf-8编码
-        fio = fio2.communicate()[0].decode("utf-8")
-
-        # 初始化列表
-        bw_num = []
-        iops_num = []
-
-        # 匹配数字和小数点，并将其元素更新
-        for line in fio.split("\n"):
-            if "bw" in line:
-                bw_num.extend(re.findall(r"\d+\.\d+|\d+", line))
-            elif "iops" in line:
-                iops_num.extend(re.findall(r"\d+\.\d+|\d+", line))
-
-        # 将str类型转换为float后再转换为int
-        bw_num = [int(float(e)) for e in bw_num]
-        iops_num = [int(float(e)) for e in iops_num]
-        # 跳过第一次运行结果
-        if i == 0:
-            continue
-        else:
-            # 格式化fio结果
-            for KorM in fio.split("\n"):
-                # 判断格式化的结果中是否存在MiB单位的值
-                if "MiB" in KorM:
-                    # 若有则转换为KiB
-                    bw[0] += bw_num[0] * 1024
-                    bw[1] += bw_num[1] * 1024
-                    bw[2] += bw_num[3] * 1024
-                    iops[0] += iops_num[0] * 1024
-                    iops[1] += iops_num[1] * 1024
-                    iops[2] += iops_num[2] * 1024
-                else:
-                    bw[0] += bw_num[0]
-                    bw[1] += bw_num[1]
-                    bw[2] += bw_num[3]
-                    iops[0] += iops_num[0]
-                    iops[1] += iops_num[1]
-                    iops[2] += iops_num[2]
-
-    bwMin = int(bw[0] / 3)
-    bwMax = int(bw[1] / 3)
-    bwAvg = int(bw[2] / 3)
-    iopsMin = int(iops[0] / 3)
-    iopsMax = int(iops[1] / 3)
-    iopsAvg = int(iops[2] / 3)
-
-    print(
-        f"随机读均值如下:\n带宽最小值:{bwMin},最大值{bwMax},均值{bwAvg}\nIOPS最小值:{iopsMin},"
-        f"最大值{iopsMax},均值{iopsAvg}"
-    )
-
-
-# 随机读写
 def randrw():
     # 初始化用于存储运行结果的列表
     bw = [0, 0, 0, 0, 0, 0]
     iops = [0, 0, 0, 0, 0, 0]
     # fio重复运行4次
     print("随机读写进行中...")
-    for i in range(4):
-        cmd = [
-            "fio",
-            "-name=YEOS",
-            "-size=32G",
-            "-runtime=120s",
-            "-bs=4k",
-            "-direct=1",
-            "-rw=randrw",
-            "-ioengine=libaio",
-            "-numjobs=12",
-            "-group_reporting",
-            "-iodepth=64",
-            f"-filename=/dev/{disk}",
-            "-rwmixwrite=30",
-        ]
+    cmd = [
+        "fio",
+        "-name=YEOS",
+        "-size=32G",
+        f"-runtime={stress_time}",
+        "-bs=4k",
+        "-direct=1",
+        "-rw=randrw",
+        "-ioengine=libaio",
+        f"-numjobs={stress_disk}",
+        "-group_reporting",
+        "-iodepth=64",
+        f"-filename={stress_dir}",
+        "-rwmixwrite=30",
+    ]
 
-        # 将fio运行结果标准输出到管道
-        fio1 = subprocess.Popen(cmd, stdout=subprocess.PIPE, shell=False)
+    # 将fio运行结果标准输出到管道
+    fio1 = subprocess.Popen(cmd, stdout=subprocess.PIPE, shell=False)
 
-        fio2 = subprocess.Popen(
-            ["grep", "samples"], stdin=fio1.stdout, stdout=subprocess.PIPE, shell=False
-        )
+    fio2 = subprocess.Popen(
+        ["grep", "samples"], stdin=fio1.stdout, stdout=subprocess.PIPE, shell=False
+    )
 
-        # 使用communicate获取子进程的标准输出并格式化为utf-8编码
-        fio = fio2.communicate()[0].decode("utf-8")
+    # 使用communicate获取子进程的标准输出并格式化为utf-8编码
+    fio = fio2.communicate()[0].decode("utf-8")
 
-        # 初始化列表
-        bw_num = []
-        iops_num = []
-        # 匹配数字和小数点，并将其元素更新
-        for line in fio.split("\n"):
-            if "bw" in line:
-                bw_num.extend(re.findall(r"\d+\.\d+|\d+", line))
-            elif "iops" in line:
-                iops_num.extend(re.findall(r"\d+\.\d+|\d+", line))
+    # 初始化列表
+    bw_num = []
+    iops_num = []
+    # 匹配数字和小数点，并将其元素更新
+    for line in fio.split("\n"):
+        if "bw" in line:
+            bw_num.extend(re.findall(r"\d+\.\d+|\d+", line))
+        elif "iops" in line:
+            iops_num.extend(re.findall(r"\d+\.\d+|\d+", line))
 
-        # 将str类型转换为float后再转换为int
-        bw_num = [int(float(e)) for e in bw_num]
-        iops_num = [int(float(e)) for e in iops_num]
+    # 将str类型转换为float后再转换为int
+    bw_num = [int(float(e)) for e in bw_num]
+    iops_num = [int(float(e)) for e in iops_num]
 
-        if i == 0:
-            continue
-        else:
-            # 读带宽
-            bw[0] += bw_num[0]
-            bw[1] += bw_num[1]
-            bw[2] += bw_num[3]
-            # 写带宽
-            bw[3] += bw_num[6]
-            bw[4] += bw_num[7]
-            bw[5] += bw_num[9]
-            # 读IOPS
-            iops[0] += iops_num[0]
-            iops[1] += iops_num[1]
-            iops[2] += iops_num[2]
-            # 写IOPS
-            iops[3] += iops_num[5]
-            iops[4] += iops_num[6]
-            iops[5] += iops_num[7]
-
-    RbwMin = int(bw[0] / 3)
-    RbwMax = int(bw[1] / 3)
-    RbwAvg = int(bw[2] / 3)
-
-    WbwMin = int(bw[3] / 3)
-    WbwMax = int(bw[4] / 3)
-    WbwAvg = int(bw[5] / 3)
-
-    RiopsMin = int(iops[3] / 3)
-    RiopsMax = int(iops[4] / 3)
-    RiopsAvg = int(iops[5] / 3)
-
-    WiopsMin = int(iops[0] / 3)
-    WiopsMax = int(iops[1] / 3)
-    WiopsAvg = int(iops[2] / 3)
     print(
         f"随机读写均值如下:\n"
-        f"读:\n带宽最小值:{RbwMin},最大值{RbwMax},均值{RbwAvg}\nIOPS最小值:{RiopsMin},"
-        f"最大值{RiopsMax},均值{RiopsAvg}"
+        f"读:\n带宽最小值:{bw_num[0]},最大值{bw_num[1]},均值{bw_num[3]}\nIOPS最小值:{iops_num[5]},"
+        f"最大值{iops_num[6]},均值{iops_num[7]}"
         "\n"
-        f"写:\n带宽最小值:{WbwMin},最大值{WbwMax},均值{WbwAvg}\nIOPS最小值:{WiopsMin},"
-        f"最大值{WiopsMax},均值{WiopsAvg}"
+        f"写:\n带宽最小值:{bw_num[6]},最大值{bw_num[7]},均值{bw_num[9]}\nIOPS最小值"
+        f":{iops_num[0]},"
+        f"最大值{iops_num[1]},均值{iops_num[2]}"
     )
 
 
-def rm_file():
-    print("请等待程序清除测试残留文件...")
-    rm = subprocess.Popen(["rm", "-rf", "/test/*"], shell=False)
-    rm.wait()
-    print("清除完毕,程序结束!")
-
-
 if __name__ == "__main__":
-    print("欢迎使用群晖测试工具\n本工具测试内容:\n设备挂载模式下IOPS性能测试")
-    disk = input("输入测试/dev/下哪个设备,例sdc,请输入:")
-    randwrite()
-    randread()
+    print(
+        """
+        欢迎使用YEOS硬盘压测工具！
+        使用本工具前，需要设置压测路径、硬盘数量、时间
+    """
+    )
+    stress_dir = input("请输入压测路径:")
+    stress_time = input("请输入压测时间[秒:s 分:m 时:h]:")
+    stress_disk = input("请输入硬盘数量:")
+    print("开始压测！\n务必等待压测结束并输出运行结果！\n不可中途停止！\n压测结束后需要删除压测所用池！")
     randrw()
